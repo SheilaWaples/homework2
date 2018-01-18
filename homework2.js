eb1b773bf9c193869201a34cd8927a6e06998664
SheilaWaples <sheilawaples78@gmail.com>
2018-01-18 09:11:03 -0500

    commit 2

--- homework2.js
+++ homework2.js
@@ -1,1 +1,11 @@
-// alert("Welcome To Black Box Beauty")
+
+
+function myFunction() {
+    document.getElementById("button1").value = "I'm Right";
+}
+
+function myFunction2(){
+	document.getElementById("button2").value = "No, I'm Right!";
+}
+
+
